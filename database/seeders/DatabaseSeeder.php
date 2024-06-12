fc2ee940591fe22669627229be6a2d36cbb43387
Khodor Hnein <151756215+khodorhnein@users.noreply.github.com>
2024-06-12 18:48:18 +0300

    Add files via upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,16 @@
 
 namespace Database\Seeders;
 
+<<<<<<< HEAD
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+=======
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\IndustryType;
+use App\Models\JobCategory;
+use App\Models\TeamSize;
+>>>>>>> 856ad1c (uptd)
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +21,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+<<<<<<< HEAD
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+=======
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call(JobCategorySeeder::class);
+        $this->call(JobRoleSeeder::class);
+        $this->call(ProfessionSeeder::class);
+        $this->call(SkillSeeder::class);
+        $this->call(ExperienceSeeder::class);
+        $this->call(IndustryTypeSeeder::class);
+        $this->call(OrganizationTypeSeeder::class);
+        $this->call(TeamSizeSeeder::class);
+        $this->call(JobSalaryTypeSeeder::class);
+        $this->call(JobEducationSeeder::class);
+        $this->call(JobTypeSeeder::class);
+        $this->call(JobTagSeeder::class);
+>>>>>>> 856ad1c (uptd)
     }
 }
